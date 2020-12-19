a14875660ff1bfdabd4e4ffc30b921f017cffa0d
Illarion-145 <73382310+Illarion-145@users.noreply.github.com>
2020-12-19 09:13:36 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	
+	int n, 
+		counter = 0, size_array = 7;
+
+	cin >> n;
+
+	int* array = new int[size_array] {1, 1, 2, 5, 1, 10, 7};
+
+	for (int i = 0; i < size_array; i++) {
+		if (array[i] >= n) {
+			counter++;
+		}
+	}
+
+	cout << counter;
+
+	return 0;
+}
