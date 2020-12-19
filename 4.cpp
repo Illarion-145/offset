ee27f54c885852732582afa465dd97b36fcb8033
Illarion-145 <73382310+Illarion-145@users.noreply.github.com>
2020-12-19 09:33:37 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int searchLessN(int* a, int length, int n) { 
+	int counter = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] < n) {
+			counter++;
+		}
+		else break;        // Т.к. последовательность отсортирована, то если i-ое число будет не меньше n, то и следующие числа будут не меньше, следовательно прерываем цикл
+	}
+	return counter;
+}
+
+int main() {
+	
+	int n,
+		size_array = 8;
+
+	cin >> n;
+
+	int* array = new int[size_array] {1, 5, 8, 10, 11, 14};
+
+	cout << searchLessN(array, size_array, n);
+
+	return 0;
+}
